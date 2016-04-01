af90987794669eacff1285fde58d8bebf139c875
David Chen <chentiki808@gmail.com>
2016-03-31 20:00:33 -0700

    refactor code, solved multiple points issue

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -19,7 +19,7 @@ module.exports = (function(){
 
 		getTweets: function(req, res){
 
-			T.get('search/tweets', { q: '%23batman OR %23trump', count: 1 }, function(err, data, response) {
+			T.get('search/tweets', { q: '%23dojobatman OR %23dojotrump', count: 1 }, function(err, data, response) {
 				var tweets = data.statuses;
 				// res.json(tweets);
 
@@ -34,18 +34,49 @@ module.exports = (function(){
 					var tweeter = tweets[i].user.name;
 					var profile_image = tweets[i].user.profile_image_url;
 
-					// adding tweets to database
-					Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+
+					// check if newly aquired tweet is in database
+					// ===================================================
+					// Tweet.find({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image}, function(err, results){
+					// 	if(err){
+					// 		console.log('Already exists');
+					// 	}
+					// 	else{
+					// 		if(results == ''){
+					// 			// create the new tweet if it doesn't exist
+					// 			Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+								
+					// 			Tweet.find({}, function(err, results){
+					// 				if(err){
+					// 					console.log('Error getting data');
+					// 				}
+					// 				else{
+					// 					res.json(results);
+					// 					console.log("printing from backend controller" + results);
+					// 				}
+					// 			});
+					// 		}
+					// 		else{
+					// 			console.log('alreay exist');
+					// 		}
+					// 	}
+					// })
+
+
+
+					//adding tweets to database
+					var results = Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+
 
 					// finds all
 					Tweet.find({}, function(err, results){
-					if(err){
-						console.log('Error getting data');
-					}
-					else{
-						res.json(results);
-					}
-				});
+						if(err){
+							console.log('Error getting data');
+						}
+						else{
+							res.json(results);
+						}
+					});
 				}
 			}); // end of T.get
 
