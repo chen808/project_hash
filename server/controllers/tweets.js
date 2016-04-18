0775af796f316699baa13827c0a93eaafe351e9e
David Chen <chentiki808@gmail.com>
2016-04-17 20:01:00 -0700

    refactor code

--- server/controllers/tweets.js
+++ server/controllers/tweets.js
@@ -18,11 +18,13 @@ module.exports = (function(){
 	return {
 
 		getTweets: function(req, res){
-
+			// %23 -> #
+			// this will search for 1 tweet at a time
 			T.get('search/tweets', { q: '%23dojobatman OR %23dojotrump', count: 1 }, function(err, data, response) {
-				var tweets = data.statuses;
-				// res.json(tweets);
 
+				// pulling the tweets from data
+				var tweets = data.statuses;
+			
 				// create hash fight id
 				var hashfight_id = "bat_v_trump"
 
@@ -37,46 +39,75 @@ module.exports = (function(){
 
 					// check if newly aquired tweet is in database
 					// ===================================================
-					// Tweet.find({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image}, function(err, results){
-					// 	if(err){
-					// 		console.log('Already exists');
-					// 	}
-					// 	else{
-					// 		if(results == ''){
-					// 			// create the new tweet if it doesn't exist
-					// 			Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+					Tweet.find({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image}, function(err, results){
+						console.log('OMG printing results before processing')
+						console.log(results);
+
+						// if result is empty, create the tweet content
+						if(results == ''){
+							// create the new tweet if it doesn't exist
+							Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+							console.log('Successfully created tweet content!');
+							// return the new tweet back to front end
+							Tweet.findOne({tweeter:tweeter, created_at:date}, function(err, results){
+								if(err){
+									console.log('error getting data');
+								}
+								else{
+									res.json(results);
+								}
+							})
+						}
+						// if result is NOT empty, do NOT create!
+						else{
+							console.log('This tweet is already in database');
+						}
+						
+
+
+
+						// else{
+						// 	if(results == ''){
+						// 		// create the new tweet if it doesn't exist
+						// 		Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
 								
-					// 			Tweet.find({}, function(err, results){
-					// 				if(err){
-					// 					console.log('Error getting data');
-					// 				}
-					// 				else{
-					// 					res.json(results);
-					// 					console.log("printing from backend controller" + results);
-					// 				}
-					// 			});
-					// 		}
-					// 		else{
-					// 			console.log('alreay exist');
-					// 		}
-					// 	}
-					// })
+						// 		Tweet.find({}, function(err, results){
+						// 			if(err){
+						// 				console.log('Error getting data');
+						// 			}
+						// 			else{
+						// 				res.json(results);
+						// 				console.log("printing from backend controller" + results);
+						// 			}
+						// 		});
+						// 	}
+						// 	else{
+						// 		console.log('already exist');
+						// 	}
+						// }
+
+
+					})
 
 
 
 					//adding tweets to database
-					var results = Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
+					// var results = Tweet.create({hashfight_id:hashfight_id, tweeter:tweeter, tweet:text, created_at:date, profile_image:profile_image});
 
 
-					// finds all
-					Tweet.find({}, function(err, results){
-						if(err){
-							console.log('Error getting data');
-						}
-						else{
-							res.json(results);
-						}
-					});
+					// // finds all return all
+					// Tweet.find({}, function(err, results){
+					// 	if(err){
+					// 		console.log('Error getting data');
+					// 	}
+					// 	else{
+					// 		res.json(results);
+					// 	}
+					// });
+
+
+
+
 				}
 			}); // end of T.get
 
